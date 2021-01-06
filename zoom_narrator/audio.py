2680bf86c026b304d600d247a64776233dbf8e8d
Daniel Maxwell-Ross <daniel@maxwell-ross.us>
2021-01-05 20:22:02 -0800

    change quoting to fix issue on windows

--- zoom_narrator/audio.py
+++ zoom_narrator/audio.py
@@ -3,4 +3,4 @@ from contextlib import AbstractContextManager
 
 
 def play(path) -> AbstractContextManager:
-    return subprocess.Popen(f"ffplay -nodisp -autoexit '{path}'", shell=True)
+    return subprocess.Popen(f'ffplay -nodisp -autoexit "{path}"', shell=True)
